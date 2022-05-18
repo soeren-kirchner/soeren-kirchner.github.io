3a1fcdab33e471248ea5aab88a8b93b09f2c4e96
Sören Kirchner <sk@tologo.de>
2022-05-18 22:38:30 +0200

    add darkmode

--- script.js
+++ script.js
@@ -4,8 +4,10 @@ let currentMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
 function setMode(darkMode) {
     if (darkMode) {
         document.body.classList.add('dark-mode')
+        darkModeToggle.src = darkModeToggle.dataset.darkmodesrc
     } else {
         document.body.classList.remove('dark-mode')
+        darkModeToggle.src = darkModeToggle.dataset.lightmodesrc
     }
 }
 
